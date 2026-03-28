6aa218598bb5ea18ca79ee9e11f024853d1cdea1
ngkhanhthien <nguyenkhanhthien2026@gmail.com>
2026-03-28 12:17:26 +0700

    chore: add modern PWA support and configure english manifest

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,9 @@ export const appConfig: ApplicationConfig = {
           }), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
           })
   ]
 };
